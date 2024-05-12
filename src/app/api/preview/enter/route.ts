945fce6c28cf55c133e3b754946c82e38da4beef
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-05-12 18:14:49 -0400

    fix: update types for api routes

--- src/app/api/preview/enter/route.ts
+++ src/app/api/preview/enter/route.ts
@@ -3,7 +3,7 @@ import { NextApiRequest } from "next";
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
-export async function GET(request: NextApiRequest) {
+export async function handler(request: NextApiRequest) {
 	if (typeof request.url !== "string") {
 		throw new Error("Expected request.url to be a string");
 	}
