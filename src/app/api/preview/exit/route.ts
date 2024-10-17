8ea5af0eeb724f0e165812a80f09f8ae6eb57c3b
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-10-17 19:25:33 -0400

    rm unused api/preview enter and exit

--- src/app/api/preview/exit/route.ts
+++
@@ -1,7 +0,0 @@
-import { draftMode } from "next/headers";
-import { redirect } from "next/navigation";
-
-export async function GET(_request: Request) {
-	draftMode().disable();
-	return redirect(`/`);
-}
