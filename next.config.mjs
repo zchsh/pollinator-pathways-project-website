b59d048228c3fd66b6d510c0769a348015557867
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-10-21 09:10:12 -0400

    condition redirect addition

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isEditable = process.env.NEXT_PUBLIC_EDITABLE === "true";
+
 const nextConfig = {
 	async redirects() {
-		return [
-			{
+		const redirectEntries = [];
+		if (isEditable) {
+			redirectEntries.push({
 				source: "/admin",
 				destination: "https://p3-website-editable.vercel.app/admin",
 				permanent: false,
-			},
-		];
+			});
+		}
+		return redirectEntries;
 	},
 	images: {
 		domains: ["assets.tina.io"],
