cf2ac0294b23f235d881eafe5acde16950ec766d
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-10-26 15:30:08 -0400

    rm editable cruft from connect page

--- src/app/connect/page-server.tsx
+++ src/app/connect/page-server.tsx
@@ -4,8 +4,10 @@ import PrimaryButtonLink from "@/components/primary-button-link";
 import { Placeholder } from "@/components/placeholder";
 import Spacer from "@/components/spacer";
 
-export default function PlaceholderPage({ data }: { data: $TSFixMe }) {
-	const { email, emailLinkText, emailSubject } = data.connect;
+const ADMIN_EMAIL = "hi@zch.sh";
+const EMAIL_SUBJECT = "Hello from your website!";
+
+export default function PlaceholderPage() {
 	return (
 		<>
 			<PagePlaceholder name="Connect">
@@ -16,20 +18,13 @@ export default function PlaceholderPage({ data }: { data: $TSFixMe }) {
 				<Placeholder>
 					<>
 						<div>Questions? Email us</div>
-						<pre>
-							<code>
-								{JSON.stringify(
-									{ email, emailLinkText, emailSubject },
-									null,
-									2
-								)}
-							</code>
-						</pre>
 						<Spacer h="2rem" />
 						<hr />
 						<Spacer h="2rem" />
-						<PrimaryButtonLink href={`mailto:${email}?subject=${emailSubject}`}>
-							{emailLinkText}
+						<PrimaryButtonLink
+							href={`mailto:${ADMIN_EMAIL}?subject=${EMAIL_SUBJECT}`}
+						>
+							Email us
 						</PrimaryButtonLink>
 					</>
 				</Placeholder>
--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,23 +1,13 @@
-import client from "@/../tina/__generated__/client";
-import { getIsEditableDeployment } from "@/lib/get-is-editable-deployment";
-import PageClient from "./page-client";
 import PageServer from "./page-server";
 import LayoutRoot from "@/components/layout-root";
 import getFooterData from "@/lib/get-footer-data";
 
-/**
- * TODO: investigate in more detail why this whole "page/-server/-client"
- * business is necessary. Seems related to TinaCMS not fully supporting
- * Next.js's `app` router.
- */
 export default async function Page() {
-	const res = await client.queries.connect({ relativePath: "connect.json" });
 	const footer = await getFooterData();
 
-	const isPreviewEnabled = getIsEditableDeployment();
 	return (
 		<LayoutRoot footer={footer}>
-			{isPreviewEnabled ? <PageClient {...res} /> : <PageServer {...res} />}
+			<PageServer />
 		</LayoutRoot>
 	);
 }
