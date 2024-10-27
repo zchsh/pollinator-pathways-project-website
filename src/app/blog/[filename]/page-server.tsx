fe9af0c77262c7db295d0bed16171f4327f4c16c
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-10-27 10:59:31 -0400

    finish blog entry wireframe

--- src/app/blog/[filename]/page-server.tsx
+++ src/app/blog/[filename]/page-server.tsx
@@ -2,8 +2,9 @@ import PagePlaceholder from "@/components/page-placeholder/page";
 import Link from "next/link";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import s from "./blog-entry.module.css";
+import { Placeholder } from "@/components/placeholder";
 
-export default function PageServer({ data }: $TSFixMe) {
+export default function PageServer({ data }: { data: $TSFixMe }) {
 	const {
 		author,
 		body,
@@ -19,24 +20,36 @@ export default function PageServer({ data }: $TSFixMe) {
 		category,
 	};
 	return (
-		<PagePlaceholder name="Blog Entry">
-			<div className={s.breadcrumbs}>
-				<Link href={`/blog`}>{`Blog`}</Link>
-				<div>{`/`}</div>
-				<div>{title}</div>
-			</div>
-			<h1>{title}</h1>
-			<br />
-			<hr />
-			<pre>
-				<code>{JSON.stringify(metadata, null, 2)}</code>
-			</pre>
-			<hr />
-			<br />
-			{coverImage && <img src={coverImage} alt={coverImageDescription} />}
-			<div className={s.blogBody}>
-				<TinaMarkdown content={body} />
-			</div>
-		</PagePlaceholder>
+		<>
+			<Placeholder name="Breadcrumbs">
+				<div className={s.breadcrumbs}>
+					<Link href={`/blog`}>{`Blog`}</Link>
+					<div>{`/`}</div>
+					<div>{title}</div>
+				</div>
+			</Placeholder>
+			<Placeholder name="Page Title">
+				<h1 style={{ textAlign: "center", padding: "1rem" }}>{title}</h1>
+			</Placeholder>
+			<Placeholder name="Author, date, & category">
+				<pre style={{ maxWidth: "400px", margin: "0 auto", padding: "1rem" }}>
+					<code>{JSON.stringify(metadata, null, 2)}</code>
+				</pre>
+			</Placeholder>
+			<Placeholder name="Cover image">
+				{coverImage ? (
+					<img
+						style={{ display: "block", margin: "0 auto" }}
+						src={coverImage}
+						alt={coverImageDescription}
+					/>
+				) : null}
+			</Placeholder>
+			<Placeholder name="Blog body">
+				<div className={s.blogBody}>
+					<TinaMarkdown content={body} />
+				</div>
+			</Placeholder>
+		</>
 	);
 }
--- src/app/blog/[filename]/page.tsx
+++ src/app/blog/[filename]/page.tsx
@@ -5,6 +5,24 @@ import PageClient from "./page-client";
 import PageServer from "./page-server";
 import LayoutRoot from "@/components/layout-root";
 import getFooterData from "@/lib/get-footer-data";
+import PagePlaceholder from "@/components/page-placeholder/page";
+import { Placeholder } from "@/components/placeholder";
+import Link from "next/link";
+
+const RECENT_POSTS = [
+	{
+		title: "Program Overview: Central London Pathway",
+		slug: "program-overview-central-london-pathway",
+	},
+	{
+		title: "Earth Day Is Everyday",
+		slug: "earth-day-is-everyday",
+	},
+	{
+		title: "Six Bees To Look Out For This Spring & Summer",
+		slug: "six-bees-to-look-out-for-this-spring-summer",
+	},
+];
 
 /**
  * TODO: investigate in more detail why this whole "page/-server/-client"
@@ -15,9 +33,46 @@ export default async function Page({ params: { filename } }: $TSFixMe) {
 	const res = await client.queries.blog({ relativePath: `${filename}.md` });
 	const footer = await getFooterData();
 	const isPreviewEnabled = getIsEditableDeployment();
+
+	/**
+	 * TODO: actually fetch recent posts
+	 */
+	const recentPosts = RECENT_POSTS;
+
 	return (
 		<LayoutRoot footer={footer} pathname="/blog">
-			{isPreviewEnabled ? <PageClient {...res} /> : <PageServer {...res} />}
+			<PagePlaceholder name="Blog Entry">
+				{isPreviewEnabled ? (
+					<PageClient
+						query={res.query}
+						variables={res.variables}
+						data={res.data}
+					/>
+				) : (
+					<PageServer data={res.data} />
+				)}
+				<Placeholder name="<BlogRecentPosts />">
+					{recentPosts?.length ? (
+						<div
+							style={{
+								maxWidth: "30em",
+								textAlign: "center",
+								margin: "0 auto",
+								padding: "2rem 1rem",
+							}}
+						>
+							<h2>Recent Posts</h2>
+							<ul style={{ listStyle: "none", padding: "0" }}>
+								{recentPosts.map((post) => (
+									<li key={post.slug}>
+										<Link href={`/blog/${post.slug}`}>{post.title}</Link>
+									</li>
+								))}
+							</ul>
+						</div>
+					) : null}
+				</Placeholder>
+			</PagePlaceholder>
 		</LayoutRoot>
 	);
 }
