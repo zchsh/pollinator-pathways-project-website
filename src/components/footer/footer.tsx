8d30ee978d4e119c15733af8f0d5449a0c19347f
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2026-02-17 20:21:55 -0500

    chore: clean up commented code in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -7,83 +7,52 @@ import VisuallyHidden from "../visually-hidden";
  * TODO: remove placeholders once all pages are wired up
  */
 function Footer({
-	subscribeHeading = "Subscribe to our newsletter",
-	subscribePlaceholder = "you@mail.com",
-	links = [],
+  subscribeHeading = "Subscribe to our newsletter",
+  subscribePlaceholder = "you@mail.com",
+  links = [],
 }: $TSFixMe) {
-	return (
-		<>
-			<div className={s.root}>
-				<div className={s.content}>
-					<EmailSubscribeForm
-						subscribeHeading={subscribeHeading}
-						subscribePlaceholder={subscribePlaceholder}
-					/>
-					<ul className={s.list}>
-						{links.map(
-							({ text, url, icon, showIconOnly }: $TSFixMe, idx: number) => (
-								<li key={idx}>
-									<PrimaryButtonLink
-										href={url}
-										target="_blank"
-										className={showIconOnly ? s.iconOnlyButton : ""}
-									>
-										<span className={s.iconAndText}>
-											<img className={s.buttonIcon} src={icon} alt="" />
-											{showIconOnly ? (
-												<VisuallyHidden>{text}</VisuallyHidden>
-											) : (
-												text
-											)}
-										</span>
-									</PrimaryButtonLink>
-								</li>
-							)
-						)}
-						{/* <li>
-						<PrimaryButtonLink
-							href="mailto:pollinatorpathwaysproject@gmail.com"
-							target="_blank"
-						>
-							<span className={s.iconAndText}>
-								<img className={s.buttonIcon} alt="" />
-								Email us
-							</span>
-						</PrimaryButtonLink>
-					</li>
-					<li>
-						<PrimaryButtonLink
-							href="https://www.instagram.com/pollinatorpathwaysproject/"
-							target="_blank"
-						>
-							<span className={s.iconAndText}>
-								<img className={s.buttonIcon} alt="" />
-								<VisuallyHidden>Instagram</VisuallyHidden>
-							</span>
-						</PrimaryButtonLink>
-					</li>
-					<li>
-						<PrimaryButtonLink
-							href="https://www.facebook.com/pollinatorpathwaysproject/"
-							target="_blank"
-						>
-							<span className={s.iconAndText}>
-								<img className={s.buttonIcon} alt="" />
-								<VisuallyHidden>Facebook</VisuallyHidden>
-							</span>
-						</PrimaryButtonLink>
-					</li> */}
-					</ul>
-				</div>
-			</div>
-			<div className={s.lowerFooterBarRoot}>
-			<div className={s.lowerFooterBar}>
-				<div>© 2024</div>
-				<a href="/admin/index.html" className={s.lowerFooterBarLink}>Edit</a>
-			</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={s.root}>
+        <div className={s.content}>
+          <EmailSubscribeForm
+            subscribeHeading={subscribeHeading}
+            subscribePlaceholder={subscribePlaceholder}
+          />
+          <ul className={s.list}>
+            {links.map(
+              ({ text, url, icon, showIconOnly }: $TSFixMe, idx: number) => (
+                <li key={idx}>
+                  <PrimaryButtonLink
+                    href={url}
+                    target="_blank"
+                    className={showIconOnly ? s.iconOnlyButton : ""}
+                  >
+                    <span className={s.iconAndText}>
+                      <img className={s.buttonIcon} src={icon} alt="" />
+                      {showIconOnly ? (
+                        <VisuallyHidden>{text}</VisuallyHidden>
+                      ) : (
+                        text
+                      )}
+                    </span>
+                  </PrimaryButtonLink>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      </div>
+      <div className={s.lowerFooterBarRoot}>
+        <div className={s.lowerFooterBar}>
+          <div>© 2024</div>
+          <a href="/admin/index.html" className={s.lowerFooterBarLink}>
+            Edit
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
