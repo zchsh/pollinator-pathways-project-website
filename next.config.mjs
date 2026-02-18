f82ec79de98d444636bff1b891fa2ac36ab744ec
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2026-02-17 20:32:45 -0500

    chore: fix editable domain name

--- next.config.mjs
+++ next.config.mjs
@@ -2,25 +2,25 @@
 const isEditable = process.env.NEXT_PUBLIC_EDITABLE === "true";
 
 const nextConfig = {
-	async redirects() {
-		const redirectEntries = [];
-		if (!isEditable) {
-			redirectEntries.push({
-				source: "/admin",
-				destination: "https://p3-website-editable.vercel.app/admin",
-				permanent: false,
-			});
-			redirectEntries.push({
-				source: "/admin/:path",
-				destination: "https://p3-website-editable.vercel.app/admin/:path",
-				permanent: false,
-			});
-		}
-		return redirectEntries;
-	},
-	images: {
-		domains: ["assets.tina.io"],
-	},
+  async redirects() {
+    const redirectEntries = [];
+    if (!isEditable) {
+      redirectEntries.push({
+        source: "/admin",
+        destination: "https://p3-editable.vercel.app/admin",
+        permanent: false,
+      });
+      redirectEntries.push({
+        source: "/admin/:path",
+        destination: "https://p3-editable.vercel.app/admin/:path",
+        permanent: false,
+      });
+    }
+    return redirectEntries;
+  },
+  images: {
+    domains: ["assets.tina.io"],
+  },
 };
 
 export default nextConfig;
