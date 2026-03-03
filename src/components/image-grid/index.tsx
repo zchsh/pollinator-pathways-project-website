f0e220e5778bb2f41b81a3a13ea5bd1e7bf53aa4
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2026-03-03 11:37:33 -0500

    feat: lightbox photo grid for projects

--- src/components/image-grid/index.tsx
+++ src/components/image-grid/index.tsx
@@ -1,29 +1,123 @@
+"use client";
+
+import { useState, useEffect, useCallback } from "react";
+import { createPortal } from "react-dom";
 import s from "./style.module.css";
+import clsx from "clsx";
 
 export function ImageGrid({ images }: { images: (string | null)[] }) {
+  const [lightboxImageIdx, setLightboxImageIdx] = useState(-1);
+  const closeLightbox = () => setLightboxImageIdx(-1);
+
   const validImages = images.filter((image) => image !== null);
-  if (!validImages.length) return null;
+  const imageCount = validImages.length;
+
+  // const lightboxNext = () => {
+  //   setLightboxImageIdx((currentIdx) =>
+  //     Math.min(currentIdx + 1, validImages.length)
+  //   );
+  // };
+  // const lightboxPrev = () => {
+  //   setLightboxImageIdx((currentIdx) => Math.max(currentIdx - 1, 0));
+  // };
+  const lightboxNext = useCallback(() => {
+    setLightboxImageIdx((currentIdx) =>
+      Math.min(currentIdx + 1, validImages.length - 1)
+    );
+  }, [validImages.length]);
+  const lightboxPrev = useCallback(() => {
+    setLightboxImageIdx((currentIdx) => Math.max(currentIdx - 1, 0));
+  }, []);
+
+  /**
+   * Listen for escape and arrow keys.
+   * On escape key, close lightbox.
+   * On arrow keys, navigate lightbox images, but
+   * only if the lightbox is open.
+   */
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (lightboxImageIdx < 0) {
+        return;
+      }
+      if (e.key === "Escape") {
+        closeLightbox();
+      }
+      if (e.key === "ArrowLeft") {
+        lightboxPrev();
+      } else if (e.key === "ArrowRight") {
+        lightboxNext();
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [lightboxImageIdx, lightboxPrev, lightboxNext]);
+
+  if (imageCount === 0) return null;
+
   return (
-    <div className={s.root}>
-      {validImages.map((image) => {
-        return (
-          <div
-            key={image}
-            style={{
-              position: "relative",
-              border: "1px solid lightgray",
-              width: "240px",
-              height: "240px",
-            }}
-          >
+    <>
+      <div className={s.photoGridRoot}>
+        {validImages.map((image, idx) => {
+          return (
+            <div className={s.photoGridItem} key={idx}>
+              <img
+                className={s.image}
+                src={image}
+                alt=""
+                onClick={() => setLightboxImageIdx(idx)}
+              />
+            </div>
+          );
+        })}
+      </div>
+
+      {lightboxImageIdx !== -1 && (
+        <ModalPortal>
+          <div className={s.lightbox}>
             <img
-              style={{ width: "100%", height: "100%", objectFit: "contain" }}
-              src={image}
+              className={clsx(s.lightboxImage)}
+              src={validImages[lightboxImageIdx]}
               alt=""
             />
+            <button
+              className={clsx(s.lightboxButton, s.lightboxClose)}
+              onClick={closeLightbox}
+            >
+              <span aria-hidden>×</span>
+            </button>
+            {lightboxImageIdx > 0 ? (
+              <button
+                className={clsx(s.lightboxButton, s.lightboxPrev)}
+                onClick={lightboxPrev}
+              >
+                <span aria-hidden>←</span>
+              </button>
+            ) : null}
+            {lightboxImageIdx < imageCount - 1 ? (
+              <button
+                className={clsx(s.lightboxButton, s.lightboxNext)}
+                onClick={lightboxNext}
+              >
+                <span aria-hidden>→</span>
+              </button>
+            ) : null}
+
+            {/*<img
+            className={s.lightboxImage}
+            src={validImages[lightboxImageIdx]}
+            alt=""
+            onClick={closeLightbox}
+          />*/}
           </div>
-        );
-      })}
-    </div>
+        </ModalPortal>
+      )}
+    </>
   );
+}
+
+function ModalPortal({ children }: { children: React.ReactNode }) {
+  return <>{createPortal(children, document.body)}</>;
 }
