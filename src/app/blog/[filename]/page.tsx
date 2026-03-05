046d4cd9c082adf70acaadbe810f7a52988dafce
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2026-03-05 17:35:08 -0500

    feat: blog covers as og images

--- src/app/blog/[filename]/page.tsx
+++ src/app/blog/[filename]/page.tsx
@@ -1,12 +1,16 @@
 import client from "@/../tina/__generated__/client";
-import { fetchBlogList } from "../utils/fetch-blog-list";
-import { getIsEditableDeployment } from "@/lib/get-is-editable-deployment";
+
 import PageClient from "./page-client";
 import PageServer from "./page-server";
 import LayoutRoot from "@/components/layout-root";
 import getSitewideData from "@/lib/get-sitewide-data";
 // Types
 import type { Metadata, ResolvingMetadata } from "next";
+// Server functions
+import path from "path";
+import sharp from "sharp";
+import { fetchBlogList } from "../utils/fetch-blog-list";
+import { getIsEditableDeployment } from "@/lib/get-is-editable-deployment";
 
 /**
  * TODO: investigate in more detail why this whole "page/-server/-client"
@@ -38,7 +42,9 @@ export default async function Page({ params: { filename } }: $TSFixMe) {
 }
 
 const METADATA_TITLE_SUFFIX = " | Pollinator Pathways Project";
-const BASE_URL = "https://www.pollinatorpathwaysproject.com";
+const BASE_URL_SHORT =
+  process.env.VERCEL_PROJECT_PRODUCTION_URL || "localhost:3000";
+const BASE_URL = `https://www.${BASE_URL_SHORT}`;
 
 export async function generateMetadata(
   { params }: { params: Promise<{ filename: string }> },
@@ -52,19 +58,27 @@ export async function generateMetadata(
   const res = await client.queries.blog({ relativePath: `${filename}.md` });
   const blogTitle = res.data.blog.title;
   const blogImage = res.data.blog.coverImage;
-  const blogImageUrl = blogImage ? `${BASE_URL}${blogImage}` : null;
-  // TODO: figure out width and height for blogImage, if applicable
   const blogImagesOpenGraph: {
     url: string;
     width?: number;
     height?: number;
-  }[] = blogImageUrl ? [{ url: blogImageUrl }] : [];
+  }[] = [];
+  if (blogImage) {
+    const blogImageFilePath = path.join(process.cwd(), "public", blogImage);
+    // Get the width and height of the image using sharp.js
+    const { width, height } = await sharp(blogImageFilePath).metadata();
+    // TODO: figure out width and height for blogImage, if applicable
+    const blogImageUrl = `${BASE_URL}${blogImage}`;
+    blogImagesOpenGraph.push({ url: blogImageUrl, width, height });
+  }
   const openGraphImages = [...previousImages, ...blogImagesOpenGraph];
+  const blogUrlAbsolute = `${BASE_URL}/blog/${filename}`;
+  console.log({ blogUrlAbsolute });
   return {
     title: blogTitle + METADATA_TITLE_SUFFIX,
     openGraph: {
       title: blogTitle,
-      url: `${BASE_URL}/blog/${filename}`,
+      url: blogUrlAbsolute,
       siteName: "Pollinator Pathways Project",
       images: openGraphImages,
       locale: "en_CA",
