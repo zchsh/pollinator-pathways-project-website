c8005cf497223df1b33410e6ac127f9f0ad41b70
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2026-03-05 15:01:54 -0500

    chore: continue work on scrappy img compression

--- scripts/optimize-images.mjs
+++ scripts/optimize-images.mjs
@@ -1,23 +1,35 @@
 import path from "path";
 
 import { gatherFiles } from "./gather-files.mjs";
+import { compressImageFile } from "./compress-image-file.mjs";
 
 const DIR_CWD = process.cwd();
-const IMAGES_DIR = path.join(DIR_CWD, "public", "uploads");
 
-const IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png"];
+const DIR_BACKUP = path.join(DIR_CWD, "old-images");
+const DIR_IMAGES = path.join(DIR_CWD, "public", "uploads");
 
-main(IMAGES_DIR);
+const IMAGE_EXTENSIONS = [".jpg", ".jpeg"];
 
-async function main(imagesDir) {
-  const [validFiles] = gatherFiles(imagesDir, (filePath) => {
+main(DIR_IMAGES, DIR_BACKUP);
+
+async function main(dirImages, dirBackup) {
+  const [validFiles] = gatherFiles(dirImages, (filePath) => {
     const extname = path.extname(filePath);
     return IMAGE_EXTENSIONS.includes(extname);
   });
 
-  console.log(validFiles);
-
   for (const imageFilePath of validFiles) {
-    await compressImageFile(imageFilePath);
+    // Back up the original file
+    const backupFilePath = path.join(
+      dirBackup,
+      path.relative(dirImages, imageFilePath)
+    );
+    const backupFileDir = path.dirname(backupFilePath);
+    if (!fs.existsSync(backupFileDir)) {
+      fs.mkdirSync(backupFileDir, { recursive: true });
+    }
+    fs.copyFileSync(imageFilePath, backupFilePath);
+    // Compress the image file
+    compressImageFile(imageFilePath, console.log);
   }
 }
